b9af411c8b1ba2f47d097f8d3952f3ab3ed9e169
Georgi Kolev <g.kolev15@gmail.com>
2023-03-20 16:49:07 +0200

    added back to home page functionality on website logo

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 export default function Navbar() {
     return (
         <div className="navbar">
             <div className="nav-container">
-                <span className="logo">Booking.com</span>
+                <Link to="/" className="logo-link">
+                    <span className="logo">Booking.com</span>
+                </Link>
                 <div className="nav-items">
                     <button className="nav-button">Register</button>
                     <button className="nav-button">Login</button>
