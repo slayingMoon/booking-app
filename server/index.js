08a1be72b7968c29ce0d588684efb1e6e432f895
Georgi Kolev <g.kolev15@gmail.com>
2023-03-23 08:50:36 +0200

    added user authentication

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import authRoute from "./routes/auth.js";
 import usersRoute from "./routes/users.js";
 import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
+import cookieParser from "cookie-parser";
 
 const app = express(); //thanks to type: module in package.json
 dotenv.config(); //configuration required in order to use env variables
@@ -25,7 +26,7 @@ mongoose.connection.on("disconnected", () => {
 });
 
 //middlewares
-
+app.use(cookieParser());
 app.use(express.json()); //required in order to send json requests to our express server
 
 app.use("/api/auth", authRoute);
@@ -33,6 +34,7 @@ app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
 
+//Error handler
 app.use((err, req, res, next) => {
     const errorStatus = err.status || 500;
     const errorMessage = err.message || "Something went wrong!";
