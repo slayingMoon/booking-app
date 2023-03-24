a980fe935234be3cf8e6d5a4b160fdfc8fe70516
Georgi Kolev <g.kolev15@gmail.com>
2023-03-24 07:36:07 +0200

    added CRUD operations logic for rooms

--- server/routes/rooms.js
+++ server/routes/rooms.js
@@ -1,5 +1,18 @@
 import express from "express";
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from "../controllers/roomController.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
+
+//CREATE
+router.post("/:hotelId", verifyAdmin, createRoom);
+//UPDATE
+router.put("/:id", verifyAdmin, updateRoom);
+//DELETE
+router.delete("/:id/:hotelId", verifyAdmin, deleteRoom);
+//GET
+router.get("/:id", getRoom);
+//GET ALL
+router.get("/", getRooms);
 
 export default router;
